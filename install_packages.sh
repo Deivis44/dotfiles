35524b1967a334dbf685b530a599707d247e5db7
juan <yo>
2024-08-24 19:13:39 -0500

    new way for installing packages

--- install_packages.sh
+++ install_packages.sh
@@ -12,6 +12,12 @@ show_error() {
     echo -e "\e[1;31m$message\e[0m"
 }
 
+# Función para mostrar un mensaje de éxito
+show_success() {
+    local message=$1
+    echo -e "\e[1;32m$message\e[0m"
+}
+
 # Función para preguntar si se desea instalar un paquete
 ask_install() {
     local pkg=$1
@@ -25,110 +31,107 @@ ask_install() {
     done
 }
 
-# Función para instalar paquetes
+# Función para instalar un paquete usando pacman o yay
+install_package() {
+    local pkg=$1
+    local install_all=$2
+
+    if pacman -Qs $pkg > /dev/null; then
+        show_info "$pkg ya está instalado."
+        return 0
+    fi
+
+    if [ "$install_all" = true ] || ask_install "$pkg"; then
+        show_info "Intentando instalar $pkg con pacman..."
+        if sudo pacman -Syu --noconfirm $pkg; then
+            show_success "$pkg instalado con éxito con pacman."
+            return 0
+        else
+            show_info "No se pudo instalar $pkg con pacman. Intentando con yay..."
+            if yay -Syu --noconfirm $pkg; then
+                show_success "$pkg instalado con éxito con yay."
+                return 0
+            else
+                show_error "Error al instalar $pkg."
+                return 1
+            fi
+        fi
+    else
+        show_info "$pkg omitido por el usuario."
+        return 2
+    fi
+}
+
+# Función para instalar un grupo de paquetes
+install_group() {
+    local group_name=$1
+    local packages=("${!2}")
+    local install_all=$3
+
+    show_info "Instalando grupo de paquetes: $group_name"
+    for pkg in "${packages[@]}"; do
+        if install_package "$pkg" "$install_all"; then
+            if pacman -Qs $pkg > /dev/null; then
+                installed+=("$pkg (pacman)")
+            else
+                installed+=("$pkg (yay)")
+            fi
+        elif [ $? -eq 2 ]; then
+            user_skipped+=("$pkg")
+        else
+            errors+=("$pkg")
+        fi
+    done
+}
+
+# Función para instalar todos los paquetes
 install_packages() {
-    local errors=()
     local install_all=$1
 
-    # Grupo 1: Herramientas de gestión de dotfiles
-    local dotfiles_tools=(
-        "stow"  # Utilizado para gestionar dotfiles con enlaces simbólicos
-        "git"   # Sistema de control de versiones para manejar dotfiles
+    # Grupos de paquetes
+    declare -A package_groups=(
+        ["Herramientas de Gestión de Dotfiles"]=("stow" "git")
+        ["Utilidades Básicas del Sistema"]=("curl" "unzip" "tree" "ranger" "spotify-launcher" "syncthing" "starship" "zathura" "zathura-pdf-mupdf" "telegram-desktop" "session-desktop" "torbrowser-launcher" "openvpn" "protonvpn-cli-ng" "virtualbox" "virtualbox-host-modules-arch" "virtualbox-guest-iso" "obs-studio")
+        ["Herramientas de Desarrollo y Python"]=("neovim" "python" "python-pynvim" "npm" "python-virtualenv" "pyright" "debugpy" "go" "base-devel" "gcc")
+        ["Tmux y Gestores de Plugins"]=("tmux" "bc" "jq" "gh" "glab" "playerctl")
+        ["Terminal y Shell"]=("kitty" "zsh")
+        ["Fuentes y Símbolos"]=("noto-fonts" "noto-fonts-extra" "noto-fonts-emoji" "ttf-nerd-fonts-symbols" "ttf-nerd-fonts-symbols-mono")
     )
 
-    # Grupo 2: Utilidades básicas del sistema
-    local system_utilities=(
-        "curl"           # Herramienta para transferir datos con URLs
-        "unzip"          # Utilidad para descomprimir archivos ZIP
-        "tree"           # Visualizacion del path en la terminal
-        "ranger"
-        "spotify-launcher"
-        "syncthing"
-        "starship"       # Prompt de shell personalizable
-        "zathura"        # Visor de PDF ligero
-        "zathura-pdf-mupdf"  # Motor para visualizar archivos PDF en Zathura
-        "telegram-desktop"  # Cliente de Telegram
-        "session-desktop"   # Cliente de mensajería Session (AUR)
-        "torbrowser-launcher"  # Navegador Tor
-        "openvpn"        # Requerido para ProtonVPN
-        "protonvpn-cli-ng"  # Cliente CLI de ProtonVPN (AUR)
-        "virtualbox"        # Aplicación de VirtualBox
-        "virtualbox-host-modules-arch"  # Módulos del kernel para VirtualBox
-        "virtualbox-guest-iso"  # Guest Additions ISO para máquinas virtuales
-        "obs-studio"     # Software para grabación y transmisión de video
-    )
+    # Inicializar arrays para el resumen
+    installed=()
+    skipped=()
+    user_skipped=()
+    errors=()
 
-    # Grupo 3: Herramientas de desarrollo y Python
-    local dev_tools=(
-        "neovim"          # Editor de texto extensible
-        "python"          # Lenguaje de programación Python
-        "python-pynvim"   # Integración de Python con Neovim
-        "npm"             # Gestor de paquetes para Node.js
-        "python-virtualenv" # Herramienta para crear entornos virtuales de Python
-        "pyright"         # LSP para Python
-        "debugpy"         # Depurador para Python
-        "go"              # Lenguaje de programación Go
-        "base-devel"      # Herramientas de desarrollo esenciales para compilación
-        "gcc"             # Compilador de C necesario para algunos plugins
-    )
-
-    # Grupo 4: Tmux y gestores de plugins
-    local tmux_tools=(
-        "tmux"  # Multiplexor de terminales
-        "bc"  # Para el widget de velocidad de red y git en Tmux
-        "jq"  # Utilidad para trabajar con JSON en la línea de comandos
-        "gh"  # GitHub CLI
-        "glab"  # GitLab CLI
-        "playerctl"  # Controlador de reproducción multimedia
-    )
-
-    # Grupo 5: Terminal y Shell
-    local terminal_shell_tools=(
-        "kitty"  # Emulador de terminal
-        "zsh"    # Shell
-    )
-
-    # Grupo 6: Fuentes y símbolos
-    local fonts_symbols=(
-        "noto-fonts"  # Fuentes Noto
-        "noto-fonts-extra"  # Fuentes Noto adicionales
-        "noto-fonts-emoji"  # Emojis Noto
-        "ttf-nerd-fonts-symbols"  # Nerd Fonts Symbols
-        "ttf-nerd-fonts-symbols-mono"  # Nerd Fonts Symbols Mono
-    )
-
-    # Instalación de paquetes en cada grupo
-    for group in dotfiles_tools system_utilities dev_tools tmux_tools terminal_shell_tools fonts_symbols; do
-        local packages=("${!group}")
-        for pkg in "${packages[@]}"; do
-            if ! pacman -Qs $pkg > /dev/null; then
-                if [ "$install_all" = true ] || ask_install "$pkg"; then
-                    show_info "$pkg no está instalado. Instalando $pkg..."
-                    if ! sudo pacman -Syu --noconfirm $pkg; then
-                        show_error "Error al instalar $pkg."
-                        errors+=("$pkg: Falló la instalación.")
-                    fi
-                else
-                    show_info "$pkg omitido por el usuario."
-                fi
-            else
-                show_info "$pkg ya está instalado."
-            fi
-        done
+    # Instalar los paquetes de cada grupo
+    for group in "${!package_groups[@]}"; do
+        install_group "$group" package_groups["$group"][@] "$install_all"
     done
 
+    # Instalación de herramientas adicionales
+    install_additional_tools
+
+    # Resumen de la instalación
+    show_summary
+}
+
+# Función para instalar herramientas adicionales (Tmux Plugin Manager, NvChad, Starship, Oh My Zsh)
+install_additional_tools() {
     # Instalación de Tmux Plugin Manager (tpm)
     show_info "Instalando Tmux Plugin Manager (tpm)..."
     if [ ! -d "$HOME/.config/tmux/plugins/tpm" ]; then
         git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
         if [ $? -ne 0 ]; then
             show_error "Error al instalar tmux plugin manager (tpm)."
-            errors+=("tpm: Falló la instalación.")
+            errors+=("tpm")
         else
-            show_info "Tmux Plugin Manager instalado con éxito."
+            show_success "Tmux Plugin Manager instalado con éxito."
+            installed+=("tpm")
         fi
     else
         show_info "Tmux Plugin Manager ya está instalado."
+        skipped+=("tpm")
     fi
 
     # Instalación de NvChad
@@ -137,12 +140,14 @@ install_packages() {
         git clone https://github.com/NvChad/starter ~/.config/nvim
         if [ $? -ne 0 ]; then
             show_error "Error al instalar NvChad."
-            errors+=("NvChad: Falló la instalación.")
+            errors+=("NvChad")
         else
-            show_info "NvChad instalado con éxito."
+            show_success "NvChad instalado con éxito."
+            installed+=("NvChad")
         fi
     else
         show_info "NvChad ya está instalado."
+        skipped+=("NvChad")
     fi
 
     # Instalación de Starship
@@ -151,12 +156,14 @@ install_packages() {
         curl -sS https://starship.rs/install.sh | sh -s -- --yes
         if [ $? -ne 0 ]; then
             show_error "Error al instalar Starship."
-            errors+=("Starship: Falló la instalación.")
+            errors+=("Starship")
         else
-            show_info "Starship instalado con éxito."
+            show_success "Starship instalado con éxito."
+            installed+=("Starship")
         fi
     else
         show_info "Starship ya está instalado."
+        skipped+=("Starship")
     fi
 
     # Instalación de Oh My Zsh
@@ -165,26 +172,38 @@ install_packages() {
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
         if [ $? -ne 0 ]; then
             show_error "Error al instalar Oh My Zsh."
-            errors+=("Oh My Zsh: Falló la instalación.")
+            errors+=("Oh My Zsh")
         else
-            show_info "Oh My Zsh instalado con éxito."
+            show_success "Oh My Zsh instalado con éxito."
+            installed+=("Oh My Zsh")
         fi
     else
         show_info "Oh My Zsh ya está instalado."
+        skipped+=("Oh My Zsh")
     fi
+}
 
-    # Resumen de errores
-    if [ ${#errors[@]} -ne 0 ]; then
-        show_error "Resumen de paquetes con errores:"
-        for err in "${errors[@]}"; do
-            show_error "$err"
-        done
-    else
-        show_info "Todos los paquetes fueron instalados con éxito."
-    fi
-
-    # Instrucciones post-instalación
-    show_info "Recuerda: Después de ejecutar el script de enlaces, abre tmux y usa Ctrl + Space + Shift + I para instalar los plugins descritos en el archivo tmux.conf."
+# Función para mostrar el resumen de la instalación
+show_summary() {
+    show_info "-------------------------------------------"
+    show_info "Resumen de la instalación"
+    show_info "-------------------------------------------"
+    show_info "Paquetes instalados:"
+    for item in "${installed[@]}"; do
+        show_success " - $item"
+    done
+    show_info "Paquetes ya estaban instalados:"
+    for item in "${skipped[@]}"; do
+        show_info " - $item"
+    done
+    show_info "Paquetes omitidos por el usuario:"
+    for item in "${user_skipped[@]}"; do
+        show_info " - $item"
+    done
+    show_info "Paquetes que fallaron:"
+    for item in "${errors[@]}"; do
+        show_error " - $item"
+    done
 }
 
 # Preguntar si se desea instalar todo de una vez o uno por uno
@@ -194,4 +213,3 @@ if [[ $install_all =~ ^[Ss]$ ]]; then
 else
     install_packages false
 fi
-
